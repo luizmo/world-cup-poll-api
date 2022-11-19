eab87df20db641bfc087941e4e5c80fb0590b77d
Luiz <luizmiguel.oliveira18@gmail.com>
2022-11-18 23:04:17 -0300

    Feat: model sequelize

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-import { openDb } from './configDB.js';
+
 import express from 'express';
 
 const app = express();
 const port = 5000;
 
-openDb();
+
 
 app.get('/', (req, res) => res.send('Copa do mundo 2022'));
+
+app.get('/games', (req, res) => res.send(
+
+));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/configDB.js
+++ src/configDB.js
@@ -1,9 +1,14 @@
-import sqlite3 from 'sqlite3'
-import { open } from 'sqlite'
+//import sqlite3 from 'sqlite3'
+//import { open } from 'sqlite'
 
-export async function openDb () {
-  return open({
-    filename: './database.db',
-    driver: sqlite3.Database
-  })
-}
+//export async function openDb () {
+//  return open({
+//    filename: './database.db',
+//    driver: sqlite3.Database
+//  })
+//}
+
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
+
+export default sequelize  
